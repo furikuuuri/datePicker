f551edae0eea269ab56801eee02420eae31e7ab4
furikuuuri <maxim03nock@mail.ru>
2022-06-21 17:52:08 +0300

    adding time select

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+
+import { Paper, Stack } from '@mui/material';
+import { Container } from '@mui/system';
 import DateComponent from './DateComponent';
 import "./styles/app.css"
+import TimeSelectComponent from './TimeSelectComponent';
 function App() {
 
  
   return (
-    <div className="app"> 
-        <DateComponent ></DateComponent> 
+    
+    
+        <Paper
+          fluid
+          maxHeight
+          elevation={24}
+          style={{
+            display:"flex",
+            alignItems:"flex-start",
+            flexDirection:"column",
+            width:"70vw",
+            height:"80vh",
+            backgroundColor:'#e6f7ff',
+            marginTop:'10vh',
+            marginLeft:'15vw'
+        }}>
+           <div>
+             Выберите дату
+           </div>
+               
+            <Container
+              style={{
+                display:"flex",
+                alignItems:"flex-start",
+                flexDirection:"row",
+            }}>
+              <DateComponent style={{
+                maxWidth:"100px"
+            }}/>               
+              <TimeSelectComponent/>
+            </Container>
         
-    </div>
+        </Paper>
+        
+     
+       
+        
+   
   );
 }
 
--- src/DateComponent.jsx
+++ src/DateComponent.jsx
@@ -1,4 +1,4 @@
-import { CalendarPicker,StaticDatePicker, LocalizationProvider, bgBG } from '@mui/x-date-pickers';
+import { CalendarPicker,StaticDatePicker, LocalizationProvider, bgBG, DatePicker } from '@mui/x-date-pickers';
 import { TextField } from '@mui/material';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { useState } from 'react';
@@ -16,15 +16,12 @@ function DateComponent() {
     
        <LocalizationProvider dateAdapter={AdapterDateFns}>
           <CalendarPicker 
-            
-            orientation="landscape"
             openTo="day"
             disableHighlightToday={true}
             minDate={new Date()}
             onChange={(value)=>onChangeDate(value)}
             value={choosenDate}
-            renderInput={(params) => 
-            <TextField {...params} />}
+            
           />
       </LocalizationProvider>
     
